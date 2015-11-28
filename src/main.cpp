0d1cf84a718a742aee2c56b0504bfe31e034f7de
Nikolay Volosatov <bamx23@yandex-team.ru>
2015-11-28 14:18:56 +0300

    Use code only for api

--- src/main.cpp
+++ src/main.cpp
@@ -1,139 +1,74 @@
 #include <fastcgi++/request.hpp>
 #include <fastcgi++/manager.hpp>
+#include <map>
 
 #include "error.hpp"
 #include "template.hpp"
 #include "json.hpp"
 
-static tstring const kTGTitle = "Taggy: Currency Converter - Currency API";
-
-class BootstrapHtml : public Template
+class CurrencyStorage
 {
-    virtual void header(tstream &out) const
-    { 
-        out << 
-            "<html lang='en'>"
-                "<head>"
-                    "<meta http-equiv='Content-Type' content='text/html; charset=utf-8' />"
-                    "<meta charset='utf-8'>"
-                    "<meta http-equiv='X-UA-Compatible' content='IE=edge'>"
-                    "<meta name='viewport' content='width=device-width, initial-scale=1'>"
-                    "<meta name='author' content='Nikolay Volosatov'>"
-                    "<title>" << kTGTitle << "</title>"
-                    "<link rel='stylesheet' href='/static/css/bootstrap.min.css' />"
-                    "<link rel='stylesheet' href='/static/css/bootstrap.theme.css' />"
-                    "<link rel='stylesheet' href='/static/css/style.css' />"
-                "</head>"
-                "<body>";
-    }
-
-    virtual void footer(tstream &out) const
-    { 
-        out << 
-                "<script src='/static/js/bootstrap.min.js'></script>"
-                "</body>"
-            "</html>";
-    }
-};
-
-class MainPageBody : public Template
-{
-    virtual void header(tstream &out) const
+public:
+    std::map<std::string, float> getCurrentCurrency()
     {
-        out << 
-            "<div class='container'>"
-                "<div class='header'>"
-                    "<nav>"
-                        "<ul class='nav nav-pills pull-right'>"
-                            "<li role='presentation' class='active'><a href='/'>API</a></li>"
-                            "<li role='presentation'><a href='http://taggy.by/'>Home</a></li>"
-                        "</ul>"
-                    "</nav>"
-                    "<h3 class='text-muted'>" << kTGTitle << "</h3>"
-                "</div>"
-                "<div class='jumbotron'>";
-    }
-
-    virtual void footer(tstream &out) const
-    { 
-        out <<
-                "</div>"
-                "<footer class='footer'>"
-                "<p>&copy; VASAG 2015</p>"
-                "</footer>"
-            "</div> <!-- /container -->"; 
+        std::map<std::string, float> result;
+        result["BYR"] = 18000;
+        result["RUB"] = 65;
+        return result;
     }
 };
 
 class MainRequest : public Fastcgipp::Request<tchar_t>
 {
-    void httpHeader() 
+    CurrencyStorage storage;
+
+    void httpHeader(bool isJson = true) 
     {
+        if (isJson) {
+            out << "Content-Type: application/json; charset=utf-8\r\n\r\n";
+        } else {
+            out << "Content-Type: text/html; charset=utf-8\r\n\r\n";
+        }
+    }
+
+    void http404()
+    {
+        out << "Status: 404 Not found\r\n";
         out << "Content-Type: text/html; charset=utf-8\r\n\r\n";
     }
 
     bool response()
     {
-        httpHeader();
+        if (environment().requestUri == "/api/v1/currency/") {
+            httpHeader();
+            auto currency = storage.getCurrentCurrency();
+            jobj currencyObj;
 
-        BootstrapHtml html;
-        MainPageBody body;
-        
-        if (environment().requestUri == "/") {
-            body << 
-                "<p>"
-                    "<a href='/rates' class='btn btn-sm btn-warning'>Latest Currency(JSON)</a> "
-                    "<a href='/history/0/24' class='btn btn-sm btn-warning'>Currency History(JSON)</a>"
-                "</p>"
-                "<h3>Actual currencies <i>(for 1 USD)</i></h3>";
-
-            body << 
-                "<table class='table' style='text-align: center'>"
-                    "<tr><td>Currency</td><td>Rate</td></tr>";
-            for (size_t i = 0; i < 20; ++i) {
-                body <<
-                    "<tr><td><strong>" << "BYR" << "</strong></td><td>" << 17500.02 << "</td></tr>";
+            for (auto& kvp : currency) {
+                currencyObj << jkvp(kvp.first, kvp.second);
             }
-            body <<
-                "</table>";
-        } else if (environment().requestUri == "/json") {
+
             out << (jobj() 
-                    << jkvp("key", 42)
-                    << jkvp("string", "str")
-                    << jkvp("broken", "ecr\\str")
-                    << jkvp("russian", "привет")
-                    << jkvp("arr", jarr() 
-                        << "hello" 
-                        << 23 
-                        << 77.01f 
-                        << -1L
-                    )
-                    << jkvp("obj", jobj() 
-                        << jkvp("inKey", 0)
-                        << jkvp("str", "World!")
-                    )
+                    << jkvp("currency", currencyObj)
+                    << jkvp("id", "20151025T000300")
+                    << jkvp("time", "20151025T000300+03")
+                    << jkvp("updated", "20151025T190300+03")
                 );
-            return true;
         } else {
-            body << "<h1>404 Not found</h1>";
+            http404();
+            out << "404 Not found";
         }
 
-        html << body;
-        out << html;
         return true;
     }
 };
 
-// The main function is easy to set up
 int main()
 {
-    try
-    {
+    try {
         Fastcgipp::Manager<MainRequest> fcgi;
         fcgi.handler();
-    }
-    catch(std::exception& e)
-    {
+    } catch(std::exception& e) {
         error_log(e.what());
     }
 }
