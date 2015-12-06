fd6ba9f455d0e49fe845b52a9600a1634f1c2d3f
Nikolay Volosatov <bamx23@yandex-team.ru>
2015-12-06 15:29:50 +0300

    Logging for Xcode

--- project/LatestCurrency/LatestCurrency/entry.cpp
+++ project/LatestCurrency/LatestCurrency/entry.cpp
@@ -1,7 +1,8 @@
+#define XCODE
+
 #include "currency_storage.hpp"
 
 int entry_point()
 {
-    std::cout << "Test\n";
     return 0;
 }
--- src/error.hpp
+++ src/error.hpp
@@ -32,12 +32,16 @@ namespace logging
     {
         using namespace std;
         using namespace boost;
+#ifndef XCODE
         static ofstream debug;
 
         if (!debug.is_open()) {
             debug.open("/var/log/fastcgipp/debug.log", ios_base::out | ios_base::app);
             debug.imbue(locale(debug.getloc(), new posix_time::time_facet()));
         }
+#else
+        auto &debug = std::cerr;
+#endif
 
         debug << (msg.isError ? "E " : "D ") << '[' << msg.time << "] " << msg.text << endl;
     }
